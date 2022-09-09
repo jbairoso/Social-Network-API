8b3fd7bd146fe73fc45686aba7cbc899d6cdd885
jbairoso <jbairoso@gmail.com>
2022-09-08 21:36:16 -0700

    update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -41,6 +41,22 @@ const userController = {
       })
       .catch((err) => res.json(err));
   },
+  
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $set: body },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = userController;
