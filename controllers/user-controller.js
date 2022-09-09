a5be5f53f0cd6fdad2e183b80f4d3904c6cda29d
jbairoso <jbairoso@gmail.com>
2022-09-08 21:27:43 -0700

    create user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,37 +1,46 @@
-const { User, Thought } = require('../models');
+const { User, Thought } = require("../models");
 
 const userController = {
- getAllUser(req, res) {
+  getAllUser(req, res) {
     User.find({})
-        .select('-__v')
-        .sort({ _id: -1 })
-        .then(dbUserData => res.json(dbUserData))
-         .catch(err => {
-            console.log(err);
-            res.sendStatus(400);
-        });
-},
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 
-getUserById({ params }, res) {
+  getUserById({ params }, res) {
     User.findOne({ _id: params.id })
-    .populate([
-        { path: 'thoughts', select: "-__v" },
-        { path: 'friends', select: "-__v" }
-    ])
-    .select('-__v')
-    .then(dbUserData => {
+      .populate([
+        { path: "thoughts", select: "-__v" },
+        { path: "friends", select: "-__v" },
+      ])
+      .select("-__v")
+      .then((dbUserData) => {
         if (!dbUserData) {
-            res.status(404).json({message: 'No user found with this id'});
-            return;
+          res.status(404).json({ message: "No user found with this id" });
+          return;
         }
         res.json(dbUserData);
-    })
-    .catch(err => {
+      })
+      .catch((err) => {
         console.log(err);
         res.status(400).json(err);
-    });
-},
-}
+      });
+  },
 
+  createUser({ params, body }, res) {
+    console.log(params);
+    User.create(body)
+      .then((dbUserData) => {
+        console.log(dbUserData);
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
 
 module.exports = userController;
